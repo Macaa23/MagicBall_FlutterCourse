f51d1778018398fadcdd7f261b8c545425283b16
Rocío Sepúlveda <rocio_23s@hotmail.com>
2020-12-17 17:55:10 +0100

    [Maca] adds code instructed by lesson 67, Step 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue.shade700,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
+}
